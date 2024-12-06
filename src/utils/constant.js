fee65d19a0863dc9c797de1880fc97729fee2d8a
skd28 <shivajaldwivedi@gmail.com>
2024-12-06 11:35:30 +0530

    first commit

---
+++ src/utils/constant.js
@@ -0,0 +1,4 @@
+export const USER_API_END_POINT="https://job-backend-beryl.vercel.app/api/user";
+export const JOB_API_END_POINT="https://job-backend-beryl.vercel.app/api/job";
+export const APPLICATION_API_END_POINT="https://job-backend-beryl.vercel.app/api/application";
+export const COMPANY_API_END_POINT="https://job-backend-beryl.vercel.app/api/company";
