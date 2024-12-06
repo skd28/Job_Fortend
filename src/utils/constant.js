76993481cf112afac54ec8939465e44b0d2b1807
skd28 <shivajaldwivedi@gmail.com>
2024-12-06 11:44:25 +0530

    build a project for vercel deploy

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -2,3 +2,4 @@ export const USER_API_END_POINT="https://job-backend-beryl.vercel.app/api/user";
 export const JOB_API_END_POINT="https://job-backend-beryl.vercel.app/api/job";
 export const APPLICATION_API_END_POINT="https://job-backend-beryl.vercel.app/api/application";
 export const COMPANY_API_END_POINT="https://job-backend-beryl.vercel.app/api/company";
+// All api end points
